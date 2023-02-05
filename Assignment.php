41c5a427d8d51fac4f5d0261649f587b457e1522
KhadizaChowdhury <khadizachowdhury70@gmail.com>
2023-02-06 04:07:04 +0600

    Submit Assignment

---
+++ Assignment.php
@@ -0,0 +1,11 @@
+<?php
+$tuition = 14569;
+
+$commission_25_p = round($tuition*.25);
+$commission_20_p = round($tuition*.20);
+$commission_15_p = round($tuition*.15);
+
+$commission = ($tuition >= 20000) ? "25% commission on $$tuition will be $$commission_25_p." : (($tuition >= 10000 && $tuition < 20000) ? "20% commission on $$tuition will be $$commission_20_p." : (($tuition >= 7000 && $tuition < 10000) ? "15% commission on $$tuition will be $$commission_15_p." : "For the $$tuition tuition fee, the commission will be invalid."));
+
+echo $commission;
+?>
