4a1c2dd6da12ac1f6789f129ce8441a9add9e02a
KhadizaChowdhury <khadizachowdhury70@gmail.com>
2023-02-06 23:29:07 +0600

    Answer

--- Assignment.php
+++ Assignment.php
@@ -1,11 +1,11 @@
 <?php
-$tuition = 6000;
+$tuition = 20001;
 
 $commission_25_p = round($tuition*.25);
 $commission_20_p = round($tuition*.20);
 $commission_15_p = round($tuition*.15);
 
-$commission = ($tuition > 20000) ? "25% commission on $$tuition will be $$commission_25_p." : (($tuition > 10000) ? "20% commission on $$tuition will be $$commission_20_p." : (($tuition > 7000) ? "15% commission on $$tuition will be $$commission_15_p." : (($tuition < 7000) ? "For the $$tuition tuition fee, the commission will be invalid." : "For the $$tuition tuition fee, the commission will be invalid.")));
+$commission = ($tuition > 20000) ? "25% = $$commission_25_p" : (($tuition > 10000) ? "20% = $$commission_20_p" : (($tuition > 7000) ? "15% = $$commission_15_p." : (($tuition < 7000) ? "invalid." : "invalid.")));
 
-echo $commission;
+echo "Commission on $$tuition will be $commission";
 ?>
